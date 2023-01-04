c833022f986346ad00db4ff003aa809ab365af0f
YuHankSong <ummumm98167@gmail.com>
2023-01-04 23:56:34 +0800

    202301041156

--- src/pages/Wish/Index.jsx
+++ src/pages/Wish/Index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { BrowserRouter, Route, Router, Switch } from "react-router-dom";
 import { Link } from "react-router-dom";
 import WishEnd from "./UI/WishEnd";
@@ -9,13 +9,51 @@ import WishMax from "./UI/WishMax";
 import WishNew from "./UI/WishNew";
 import WishTure from "./UI/WishTrue";
 
+const wishArticle = [{}, {}];
+
 function Wish() {
+  const [isPlzShow, setisPlzShow] = useState(false);
+  const togleModal = () => {
+    setisPlzShow(!isPlzShow);
+  };
+  let showPlz = isPlzShow ? "flex" : "none";
+
+  const [pages, setPages] = useState([
+    "熱門許願",
+    "最新許願",
+    "最多集氣",
+    "即將截止",
+    "許願成真",
+  ]);
+  // const randomNumber = useMemo(() => {
+  //   const number = Math.round(Math.random() * 10);
+  //   return number > pages.length ? 1 : number;
+  // }, [pages]);
+
+  const [current, setcurrent] = useState(() => {
+    const number = Math.round(Math.random() * 10);
+    return number > pages.length ? 1 : number;
+  });
+
   return (
     <>
       <div id="wish-wrap">
         <div className="wish-container">
           <div className="wish-chat">
-            <WishBar />
+            <div className="wish-bar">
+              {pages.map((page, index) => (
+                <Link
+                  onClick={() => {
+                    setcurrent(index);
+                  }}
+                >
+                  {page}
+                </Link>
+              ))}
+            </div>
+
+            {/* {pages[current]} */}
+            {/* <WishBar />
             <Switch>
               <Route path="/selectgo/Wish/hot" component={WishHot} />
               <Route path="/selectgo/Wish/New" component={WishNew} />
@@ -23,7 +61,7 @@ function Wish() {
               <Route path="/selectgo/Wish/End" component={WishEnd} />
               <Route path="/selectgo/Wish/Ture" component={WishTure} />
               <Route path="/selectgo/Wish/" component={WishHot} />
-            </Switch>
+            </Switch> */}
           </div>
 
           <div className="wish-list">
@@ -35,10 +73,11 @@ function Wish() {
               <br />
               正式開賣許願者即享 8 折優惠
             </p>
-            <input type="button" value="發起許願" />
+            <input type="button" onClick={togleModal} value="發起許願" />
           </div>
         </div>
       </div>
+      {/* <div style={{ display: showPlz }}></div> */}
     </>
   );
 }
