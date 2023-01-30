91a5c64a20320b5628b71ac7854ceb19407ee89b
Alex <aliceric27@gmail.com>
2023-01-30 11:30:38 +0800

    Delete somthing

--- src/pages/Cart/Index.jsx
+++ src/pages/Cart/Index.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import Styles from "../../styles/Cart.module.scss";
-import axios from "axios";
-import { CSSTransition } from "react-transition-group";
 const Cart = () => {
   const NT$ = "NT$";
   const USD$ = "USD$";
