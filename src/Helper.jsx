ad753e1fa06ea15319c45a1ebfe0612590a9fe10
Johnny Chang <johnny22375@gmail.com>
2023-02-01 14:34:37 +0800

    made final changes to chatbot

--- src/Helper.jsx
+++ src/Helper.jsx
@@ -5,15 +5,15 @@ const Helper = () => {
     let user = JSON.parse(localStorage.getItem('user'));
     // all available props
     const theme = {
-        background: '#fff',
+        background: 'white',
         fontFamily: 'Helvetica Neue',
         headerBgColor: '#F38243',
         headerFontColor: 'white',
-        headerFontSize: '20px',
+        headerFontSize: '18px',
         botBubbleColor: '#F38243',
         botFontColor: 'white',
-        // userBubbleColor: 'lightgray',
-        userFontColor: '#4a4a4a',
+        userBubbleColor: '#FAF9F6',
+        userFontColor: 'black',
     };
     const wish = '登入後進入「許願池」，點擊「發起許願」。只要你有想要的日本好物，都可以許願哦！';
     const collect = '許願完成後將會有 14 天時間讓大家集氣，集氣結束後，只要集氣數超過 50 的願望，我們就會進行評估，並找相關原廠商洽談！'
@@ -41,7 +41,7 @@ const Helper = () => {
                     {
                         id: 'questionOptions',
                         options: [
-                            { value: 1, label: '許願是什麼？', trigger: 'wish' },
+                            { value: 1, label: '許願是什麼?', trigger: 'wish' },
                             { value: 2, label: '集氣是什麼？', trigger: 'collect' },
                             { value: 3, label: '什麼時候開賣？', trigger: 'sell' },
                             { value: 4, label: '沒問題了！', trigger: 'bye' },
@@ -64,15 +64,24 @@ const Helper = () => {
                     },
                     {
                         id: 'bye',
-                        message: '掰掰！有問題再問我！:D',
-                        end: true
+                        message: '掰掰～有問題可以再來再問我呦！😁',
+                        trigger: 'end'
+                    },
+                    {
+                        id: 'end',
+                        component: (
+                            <div>
+                                <a href="/" className='mr-5'>繼續逛逛</a>
+                                <a href="/member">加入我們</a>
+                            </div>
+                        ),
+                        end: true,
                     },
                 ]}
-
                 floating={true}
-                // floating={user ? true : false}
                 placeholder={'請輸入文字'}
-                userAvatar={user && user.photoURL ? user.photoURL : '/imgs/icon.jpg'}
+                botAvatar={'/imgs/botAvatar.webp'}
+                userAvatar={user && user.photoURL ? user.photoURL : '/imgs/avatar.png'}
 
             />
         </ThemeProvider>
